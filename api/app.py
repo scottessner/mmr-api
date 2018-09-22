96791cbd3f9f49ede1da0c85cd333a1e3cd90ac4
Scott Essner <scott.essner@gmail.com>
2018-09-21 23:43:41 -0500

    Moved empty db and updated app.py to reflect

--- api/app.py
+++ api/app.py
@@ -8,7 +8,7 @@ import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/mmr-api.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mmr-api.db'
 db = SQLAlchemy(app)
 
 
