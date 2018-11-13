2226b311fe46fb374846c1509571e06838eaabc5
Scott Essner <scott.essner@gmail.com>
2018-11-13 00:59:13 -0600

    Added frontend build

--- nginx/build/service-worker.js
+++ nginx/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a912eb32d24de7a950eb07f73c4d7281.js"
+  "./precache-manifest.1d6afefc1986da7f95274f1cf53a8078.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
